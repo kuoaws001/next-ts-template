ec6f46b9c2ca2467e44f4c4e82461931dda8fb86
chris.kuo <126221813+beyondcars@users.noreply.github.com>
2024-03-30 15:35:52 +0800

    NextAuth

---
+++ app/components/NavBar.tsx
@@ -0,0 +1,18 @@
+'use client'
+
+import React from 'react'
+import Link from 'next/link'
+import styles from './NavBar.module.css'
+import { useSession } from 'next-auth/react'
+
+export const NavBar = () => {
+  const { status, data } = useSession()
+
+  return (
+    <div className={styles.header}>
+      {status === 'loading' && <div></div>}
+      {status === 'authenticated' && <div>{data.user!.name}</div>}
+      {status === 'unauthenticated' && <Link href='/api/auth/signin'>Login</Link>}
+    </div>
+  )
+}
